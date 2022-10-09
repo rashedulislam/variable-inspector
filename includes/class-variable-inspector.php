a0029943fc4f55f1b79cce0fa8becc61b05cc18a
Bowo <wibowosulistio@gmail.com>
2022-10-09 13:20:58 +0700

    Dequeue public css and js
    As they are unused

--- includes/class-variable-inspector.php
+++ includes/class-variable-inspector.php
@@ -207,8 +207,8 @@ class Variable_Inspector {
 
 		$plugin_public = new Variable_Inspector_Public( $this->get_plugin_name(), $this->get_version() );
 
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
+		// $this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
+		// $this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 
 	}
 
